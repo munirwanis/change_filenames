cc5e45583d95742e3b7d3eb77a7cdd8e52dee951
Munir Wanis <fallen.angel.pp@gmail.com>
2016-05-12 19:04:26 -0300

    :art: :memo: Adds script and readme

---
+++ README.rb
@@ -0,0 +1,9 @@
+## Change multiple file names
+
+Example on how to use:
+
+```shell
+./multiple_filename_changer.sh Jonny John txt
+```
+
+Will change all .txt files on the folder that contains the word Jonny to Jonh.
---
+++ multiple_filename_changer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# arguments
+name_to_change="$1"
+new_name="$2"
+file_type="$3" # txt, jpg, png...
+
+if [ "" = "$name_to_change" ]; then
+    echo "Example on how to use:"
+    echo "./multiple_filename_changer.sh Jonny John txt"
+    echo "Will change all .txt files on the folder that contains the word Jonny to Jonh."
+else
+    for f in *.${file_type}; do mv "$f" "`echo $f | sed s/${name_to_change}/${new_name}/`"; done
+fi
